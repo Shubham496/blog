a6a2bd583be2cf23663155f61dc2706ca312b5fe
Shubham <iatco5h@gmail.com>
2025-11-12 14:18:43 +0530

    templates

---
+++ basicApp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    name = "shubham"
+    age = 25
+    dogs = ['dog1', 'dog2']
+    info = {"b.tech": 'RJIT', "job": "netmax"}
+    isActive = True
+    di = {'name': name, "age": age, 'dogs': dogs, 'info': info, 'active': isActive}
+    return render(request, "base.html", context= di)
+
+
+
+def about(request):
+    return render(request, 'about.html')
--- myApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path("", product),
-    path("1/", product1),
-    path("2/", product1)
-]
